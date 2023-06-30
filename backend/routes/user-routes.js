b5cf2cb89d6d3085a133445c5cfe76305d2d540f
pasindu <pasindugura123@gmail.com>
2023-06-30 08:21:20 +0530

    removed user-route in main

--- backend/routes/user-routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import {login} from "../controllers/user-controllers.js";
-const user_route = express.Router();
-
-user_route.post('/login',login);
-
-export default user_route;
