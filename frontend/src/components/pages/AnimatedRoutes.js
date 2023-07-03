33af2b86647c2dd2ca52de50718436b72ec12aa8
gayya20 <92713181+gayya20@users.noreply.github.com>
2023-07-03 19:44:58 +0530

    add new 3 folders

---
+++ frontend/src/components/pages/AnimatedRoutes.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {Route,Routes,useLocation} from "react-router-dom"
+// import Menu from '../pages/Menu'
+// import Slider from '../pages/Slider'
+// import About from '../pages/About';
+// import Contact from '../pages/Contact';
+import Home from '../pages/Home'
+import Login from "../pages/Login"
+// import Login from  '../pages/Login';
+// import VideoBackground from '../pages/VideoBackground';
+// import {AnimatePresence}from 'framer-motion'
+
+
+function AnimatedRoutes() {
+    const location=useLocation();
+
+  return (
+    // <AnimatePresence>
+    <Routes  location={location} key={location.pathname}>
+       <Route path="/" element={<Home/>}/>
+    
+    <Route path="/Login" element={<Login/>}/>
+    {/* <Route path="/VideoBackground" element={<VideoBackground/>}/> */}
+  </Routes>
+  // </AnimatePresence>
+  )
+}
+
+export default AnimatedRoutes
---
+++ frontend/src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+import Slider from '../pages/Slider'
+
+
+function Home() {
+
+  return (
+  
+    <div>
+
+      <Slider/>
+    </div>
+  )
+}
+
+export default Home
