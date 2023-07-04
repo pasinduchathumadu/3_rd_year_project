4260951b9b96d3b73d8a1e27ef03c47c9a31d061
pasindu <pasindugura123@gmail.com>
2023-07-04 19:59:13 +0530

    I make changes of database.js file

--- backend/database.js
+++ backend/database.js
@@ -1,16 +1,18 @@
+import bcryptjs from "bcryptjs"
+
 import { createPool } from 'mysql';
 export const db = createPool({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"pet_care"
-})
-db.query(`select *from all_user`,(err,result)=>{
-    if(err){
-        console.log("Database Is Not Connected!!!");
-    }
-    else{
-        console.log("Established the database connection successfully!!!!");
-    }
+    host:"sql6.freesqldatabase.com",
+    user:"sql6630302",
+    password:"ItT6U6JiGh",
+    database:"sql6630302"
 })
 
+db.getConnection((err,connection)=>{
+    if (err) {
+        console.error('Error connecting to MySQL database:', err);
+        return;
+      }
+      console.log('Connected to MySQL database!');
+
+})
