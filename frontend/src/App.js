648a3968a273800fe4f40d0857dc0d9c9cd831d3
pasindu <pasindugura123@gmail.com>
2023-07-12 19:19:52 +0530

    added files

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Forgot from "./pages/Common/Forget";
+import Signup  from "./pages/Common/Signup"
+import Blogs from "./pages/Client/Payment";
+import Login from "./pages/Common/Login";
+import Home from  "./pages/Client/Home";
+import Email from "./pages/Common/Email";
+
+
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [issignup,setIssignup] = useState(false)
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+  const handleSignup = () => {
+    setIssignup(true);
+  }
+  return (
+    <React.Fragment>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/email" element={issignup ?<Email /> : <Navigate to ="/signup"/>}/>
+          <Route path="/forget" element={<Forgot />} />
+          <Route path="/signup" element={<Signup onSignup={handleSignup}/>} />
+          <Route path="/blogs" element={isLoggedIn ? <Blogs /> : <Navigate to="/login" />} />
+          <Route path="/login" element={<Login onLogin={handleLogin} />} />
+        </Routes>
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
+
+
