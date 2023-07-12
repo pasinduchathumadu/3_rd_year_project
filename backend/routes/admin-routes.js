020b09ddb71b10a4dc567b4f6dedc9cfe9d99a4f
pasinduchathumadu <pasindugura123@gmail.com>
2023-07-12 20:23:44 +0530

    Delete backend directory

--- backend/routes/admin-routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import {registration } from "../controllers/admin-controllers.js"
-const admin_route = express.Router()
-
-admin_route.post('/registration',registration)
-
-export default admin_route;
--- backend/routes/payment-routes.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import {card} from "../controllers/payment-controllers.js"
-const payment_route = express.Router();
-payment_route.post('/confirm',card);
-
-export default payment_route;
