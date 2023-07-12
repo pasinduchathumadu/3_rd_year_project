34cc095036f22e8009685d3ca36ef496b7cd6d1a
pasinduchathumadu <pasindugura123@gmail.com>
2023-07-12 21:07:03 +0530

    Delete backend directory

--- backend/routes/email-routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import {verify} from "../controllers/email-controllers.js"
-const email_route = express.Router();
-
-email_route.post('/verify',verify);
-
-export default email_route;
