b13a15ff1fa1f74cf7ab27f45be5ac57fc8995dd
pasinduchathumadu <pasindugura123@gmail.com>
2023-07-12 21:11:10 +0530

    Delete backend directory

--- backend/routes/pdf-routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import {card} from "../controllers/pdf-controllers.js"
-const pdf_route = express.Router();
-
-pdf_route.post('/card',card);
-
-export default pdf_route;
