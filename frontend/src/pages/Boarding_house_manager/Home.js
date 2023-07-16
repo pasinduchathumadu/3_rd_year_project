31d9d12645054836342188b329f4a1b3f93420e9
Hashini Thilinika <hashinithilinika.av@gmail.com>
2023-07-16 22:15:32 +0530

    boarding house dahsboard

--- frontend/src/pages/Boarding_house_manager/Home.js
+++ frontend/src/pages/Boarding_house_manager/Home.js
@@ -1,15 +1,59 @@
 import React from "react";
-// import Header from "../../components/Layout/Header";
+import BoardingStyles from '../../styles/Boarding_house_manager/Home.css';
+import HomeHeader from "../Common/HomeHeader";
+
 // import SideMenu from "../../components/Layout/BoardingSideMenu";
 
 const Home = () => {
     return (
-        <div>
-            {/* <Header /> */}
+        <div className="home-container">
+            <HomeHeader />
             {/* <SideMenu /> */}
-            Dashboard
-           
+
+            <div className="boarding-wrapper-main">
+                <div className="boarding-wrapper">
+                    <h3>Analytical Overview</h3>
+                    <div className="boarding-wrapper-box-mian">
+                        <div className="boarding-wrapper-box">
+                            <h1>10</h1>
+                            <p>Current Boarding Pets</p>
+                        </div>
+                        <div className="boarding-wrapper-box">
+                            <h1>06</h1>
+                            <p>Completed Requests</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="boarding-wrapper">
+                    <h3>Pending Boarding Requests</h3>
+                    <div className="bording-pending-box">
+                        <p>Request ID : 02</p>
+                    </div>
+                    <div className="bording-pending-box">
+                        <p>Request ID : 03</p>
+                    </div>
+                    <div className="bording-pending-box">
+                        <p>Request ID : 04</p>
+                    </div>
+                </div>
             </div>
+
+            
+            <div className="boarding-wrapper-main">
+                <div className="boarding-wrapper">
+                    <h3>Pet Analyze</h3>
+                </div>
+
+
+                <div className="boarding-wrapper">
+                    <h3>Packages</h3>
+                   
+                    
+                </div>
+            </div>
+           
+        </div>
     )
 }
 
