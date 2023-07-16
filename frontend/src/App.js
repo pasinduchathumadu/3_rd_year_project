3349e8411fa1a0a9fdce1fafdbbbaa07f5afeed2
gayya20 <92713181+gayya20@users.noreply.github.com>
2023-07-16 20:52:52 +0530

    edit dasboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,9 @@ import Blogs from "./pages/Client/Payment";
 import Login from "./pages/Common/Login";
 import Home from  "./pages/Client/Home";
 import Email from "./pages/Common/Email";
+import { Dashboard } from "./pages/Client/Dashboard";
+import { Reports } from "./pages/Client/Reports";
+import PrimarySearchAppBar from "./components/Layout/Header";
 
 
 function App() {
@@ -21,13 +24,16 @@ function App() {
   return (
     <React.Fragment>
       <main>
+        <PrimarySearchAppBar/>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Dashboard/>} />
           <Route path="/email" element={issignup ?<Email /> : <Navigate to ="/signup"/>}/>
           <Route path="/forget" element={<Forgot />} />
           <Route path="/signup" element={<Signup onSignup={handleSignup}/>} />
           <Route path="/blogs" element={isLoggedIn ? <Blogs /> : <Navigate to="/login" />} />
           <Route path="/login" element={<Login onLogin={handleLogin} />} />
+          <Route path="/reports" element={<Reports/>}></Route>
+          <Route path="/dashboard" element={<Dashboard/>}></Route>
         </Routes>
       </main>
     </React.Fragment>
--- frontend/src/components/Layout/Sidebar.js
+++ frontend/src/components/Layout/Sidebar.js
@@ -1,68 +1,3 @@
-// import * as React from "react"
-// import Box from "@mui/material/Box"
-// import Drawer from "@mui/material/Drawer"
-// import Button from "@mui/material/Button"
-// import List from "@mui/material/List"
-// import Divider from "@mui/material/Divider"
-// import ListItem from "@mui/material/ListItem"
-// import ListItemButton from "@mui/material/ListItemButton"
-// import ListItemIcon from "@mui/material/ListItemIcon"
-// import ListItemText from "@mui/material/ListItemText"
-// import InboxIcon from "@mui/icons-material/MoveToInbox"
-// import MailIcon from "@mui/icons-material/Mail"
-
-// export default function TemporaryDrawer({ state, setState, toggleDrawer }) {
-//   const list = (anchor) => (
-//     <Box
-//       sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250,backgroundColor: "black", color: "white" }}
-//       role="presentation"
-//       onClick={toggleDrawer(anchor, false)}
-//       onKeyDown={toggleDrawer(anchor, false)}
-//     >
-//       <List>
-//         {["Dashboard", "Analytics", "Reports", "News"].map((text, index) => (
-//           <ListItem key={text} disablePadding>
-//             <ListItemButton href={`/${text.toLowerCase()}`}>
-//               <ListItemIcon sx={{color:"white"}}>
-//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-//               </ListItemIcon>
-//               <ListItemText primary={text} />
-//             </ListItemButton>
-//           </ListItem>
-//         ))}
-//       </List>
-//       <Divider />
-//       <List sx={{color:"white"}}>
-//         {["All mail", "Trash", "Spam"].map((text, index) => (
-//           <ListItem key={text} disablePadding sx={{color:"white"}}>
-//             <ListItemButton sx={{color:"white"}}>
-//               <ListItemIcon sx={{color:"white"}}>
-//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-//               </ListItemIcon>
-//               <ListItemText primary={text} />
-//             </ListItemButton>
-//           </ListItem>
-//         ))}
-//       </List>
-//     </Box>
-//   )
-
-//   return (
-//     <div  sx={{backgroundColor: "black"}}>
-//       {["left"].map((anchor) => (
-//         <React.Fragment key={anchor}>
-//           <Drawer
-//             anchor={anchor}
-//             open={state[anchor]}
-//             onClose={toggleDrawer(anchor, false)}
-//           >
-//             {list(anchor)}
-//           </Drawer>
-//         </React.Fragment>
-//       ))}
-//     </div>
-//   )
-// }
 
 
 import * as React from "react";
@@ -122,7 +57,7 @@ export default function TemporaryDrawer({ state, setState, toggleDrawer }) {
       <List sx={{ backgroundColor: "black", color: "white" }}>
         {["All mail", "Trash", "Spam"].map((text, index) => (
           <ListItem key={text} disablePadding>
-            <ListItemButton >
+            <ListItemButton href={`/${text.toLocaleLowerCase()}`}>
               <ListItemIcon sx={{ color: "white" }}>
                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
               </ListItemIcon>
