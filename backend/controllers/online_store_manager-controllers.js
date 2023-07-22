82726d1e23c51678b733a328789317966e51fda9
pasindu <pasindugura123@gmail.com>
2023-07-22 18:45:51 +0530

    complete the update cart

--- backend/controllers/online_store_manager-controllers.js
+++ backend/controllers/online_store_manager-controllers.js
@@ -1,40 +1,69 @@
 
 import { db } from '../database.js'
 
+export const get_item = async (req, res, next) => {
+    const id = req.params.id;
+    console.log(id)
+    var item_catogery = ""
+    if (id === '0') {
+        item_catogery = "foods";
+    }
+    if (id === '1') {
+        item_catogery = "accessories"
+    }
+    if (id === '2') {
+        item_catogery = "others"
+    }
 
-export const add_store = async (req, res, next) => {
-    const { name,price,description,quantity,item,Categories,image } = req.body
+    const sqlQuery = 'select *from item where catogories = ?'
     const values = [
-        
-      name,
-      price,
-      image,
-      description,
-      quantity,
-      item,
-      Categories
+        item_catogery
     ]
-  
- 
-        const sqlQuery1 = "INSERT INTO item (name,unit_price,image,description,quantity,item,catogories) VALUES (?,?,?,?,?,?,?)"
-        db.query(sqlQuery1, values, (err,data) => {
-            if (err) {
-                return res.json({ message: "There is an internal error" })
-            }
+    db.query(sqlQuery, values, (err, data) => {
+        if (err) {
+            return res.json({ message: 'There is an internal error' })
+        }
+        else if (data.length < 1) {
+            return res.json({ message: 'No records' })
+        }
+        return res.json({ data })
+    })
+}
+export const add_store = async (req, res, next) => {
+    const { name, price, description, quantity, item, Categories, image } = req.body
+    const values = [
 
-            return res.json({ message: "added successfully" })
-        })
-    
+        name,
+        price,
+        image,
+        description,
+        quantity,
+        item,
+        Categories
+    ]
+
+
+    const sqlQuery1 = "INSERT INTO item (name,unit_price,image,description,quantity,item,catogories) VALUES (?,?,?,?,?,?,?)"
+    db.query(sqlQuery1, values, (err, data) => {
+        if (err) {
+            return res.json({ message: "There is an internal error" })
+        }
+
+        return res.json({ message: "added successfully" })
+    })
+
 }
 
 export const update_store = async (req, res, next) => {
-    const { item_code, price, quantity } = req.body
+    const { id, updatedescription, updateavailibility, updateprice } = req.body
+   
     const values = [
-        item_code,
-        price,
-        quantity
+        updateprice,
+        updateavailibility,
+        updatedescription,
+        id
     ]
-    const sqlQuery = 'UPDATE TABLE SET price = ? , quantity = ? WHERE item_code = ? '
+    const sqlQuery = 'UPDATE item SET unit_price = ? , quantity = ?,description = ? WHERE item_id = ? '
 
     db.query(sqlQuery, values, (err, data) => {
         if (err) {
@@ -75,7 +104,20 @@ export const view_complain = async (req, res, next) => {
         return res.json({ data })
     })
 }
+export const get_cart = async (req, res, next) => {
+    const id = req.params.id
+    const sqlQuery = "select *from item where item_id = ?"
+    const values = [
+        id
+    ]
+    db.query(sqlQuery, values, (err, data) => {
+        if (err) {
+            return res.json({ message: 'there is an error' })
+        }
+        return res.json({ data })
+    })
 
+}
 export const handling_complain = async (req, res, next) => {
     const { user_id, response_txt } = req.body
 
