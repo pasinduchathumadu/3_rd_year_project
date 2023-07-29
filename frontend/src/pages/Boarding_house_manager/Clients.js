8a1e8d3ba5c4af2e63c41b548f4d678929f81215
Hashini Thilinika <hashinithilinika.av@gmail.com>
2023-07-29 19:06:02 +0530

    boarding - clients part done

--- frontend/src/pages/Boarding_house_manager/Clients.js
+++ frontend/src/pages/Boarding_house_manager/Clients.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
-import  '../../styles/Boarding_house_manager/Home.css';
+import '../../styles/Boarding_house_manager/Home.css';
 import Header from "../../components/Layout/Header";
 import ProfilePicture from '../../assests/profile-picture.png';
+import PetImage from '../../assests/blog-1.png';
+import OwnerImage from '../../assests/profile-picture.png';
+import BankSlip from '../../assests/idli.jpg';
 
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import { styled } from '@mui/material/styles';
@@ -19,6 +22,7 @@ import Select from '@mui/material/Select';
 import Box from '@mui/material/Box';
 import { Tab } from "@mui/material";
 import { Tabs } from "@mui/material";
+import { FormLabel, TextField } from "@mui/material";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -64,16 +68,16 @@ const secondrows = [
     createDataRows(5, 'John Bye', '2023-07-10 to 2023-07-12', '16:00:00', '1500.00', 'cancelled'),
 ];
 
-function createRefundData(rfid,rqid, name,date, payment, rqStatus, rfStatus) {
-    return {rfid, rqid, name,date, payment, rqStatus, rfStatus};
+function createRefundData(rfid, rqid, name, date, payment, rqStatus, rfStatus) {
+    return { rfid, rqid, name, date, payment, rqStatus, rfStatus };
 
 }
 
 const refundrows = [
-    createRefundData(1,1,'John Deo', '2023-07-10', '1200.00', 'cancelled', 'pending'),
-    createRefundData(2,2,'John Ziya', '2023-07-12', '1200.00', 'incomplete', 'pending'),
-    createRefundData(3,3,'Jonathan Perera', '2023-07-13', '1200.00', 'cancelled', 'completed'),
-    createRefundData(4,4,'Gulio Dias', '2023-07-11', '1200.00', 'incomplete', 'completed'),
+    createRefundData(1, 1, 'John Deo', '2023-07-10', '1200.00', 'cancelled', 'pending'),
+    createRefundData(2, 2, 'John Ziya', '2023-07-12', '1200.00', 'incomplete', 'pending'),
+    createRefundData(3, 3, 'Jonathan Perera', '2023-07-13', '1200.00', 'cancelled', 'completed'),
+    createRefundData(4, 4, 'Gulio Dias', '2023-07-11', '1200.00', 'incomplete', 'completed'),
 ];
 
 const Clients = () => {
@@ -82,11 +86,50 @@ const Clients = () => {
     const handleChange = (event) => {
         setClients(event.target.value);
     };
-  
+
     const [showRequests, setShowRequests] = useState(0);
-    const handleForm = (event,existing_value) => {
+    const handleForm = (event, existing_value) => {
         setShowRequests(existing_value)
     };
+
+    const [pet, setPet] = useState(false);
+    const [addRefund, setaddRefund] = useState(false);
+    const [viewRefund, setviewRefund] = useState(false);
+
+    // click on view pet details button
+    const viewPet = () => {
+        setShowRequests(false);
+        setPet(true);
+    }
+
+    // after viewing  pet details 
+    const FinishPetViewing = () => {
+        setPet(false);
+        setShowRequests(0);
+    }
+
+    // click on add refund
+    const RefundAdding = () => {
+        setShowRequests(false);
+        setaddRefund(true);
+    }
+
+    // after click on place refund
+    const PlaceRefund = () => {
+        setaddRefund(false);
+        setShowRequests(1);
+    }
+
+    // click on view refund details
+    const ViewRefundDetails = () => {
+        setShowRequests(false);
+        setviewRefund(true);
+    }
+    // after viewing the refund details
+    const FinishRefundViewing = () => {
+        setviewRefund(false);
+        setShowRequests(1);
+    }
 
     return (
         <div className="home-container">
@@ -103,25 +146,25 @@ const Clients = () => {
                 </div>
             </div>
 
-            <Box sx={{width:'98%', marginTop:'10px', marginBottom:'10px', marginLeft:'20px', marginRight:'10px', paddingRight:'10px', paddingLeft:'10px'}}>
+            <Box sx={{ width: '98%', marginTop: '10px', marginBottom: '10px', marginLeft: '20px', marginRight: '10px', paddingRight: '10px', paddingLeft: '10px' }}>
                 <Tabs
-                value={showRequests}
-                variant="fullWidth"
-                aria-label="Tab Component"
-                onChange={handleForm}
-                indicatorColor="transparent"
-                sx={{borderRadius:'10px'}}
+                    value={showRequests}
+                    variant="fullWidth"
+                    aria-label="Tab Component"
+                    onChange={handleForm}
+                    indicatorColor="transparent"
+                    sx={{ borderRadius: '10px' }}
                 >
 
-                    <Tab sx={{backgroundColor:showRequests === 0 ? 'orange':'#F0F0F5',color:'black'}} label="Clients' Request" ></Tab>
-                    <Tab sx={{backgroundColor:showRequests === 1 ? 'orange':'#F0F0F5',color:'black'}} label="Refund Requests"></Tab>
-                    <Tab sx={{backgroundColor:showRequests === 2 ? 'orange':'#F0F0F5',color:'black'}} label="Clients"></Tab>
+                    <Tab sx={{ backgroundColor: showRequests === 0 ? 'orange' : '#F0F0F5', color: 'black' }} label="Clients' Request" ></Tab>
+                    <Tab sx={{ backgroundColor: showRequests === 1 ? 'orange' : '#F0F0F5', color: 'black' }} label="Refund Requests"></Tab>
+                    <Tab sx={{ backgroundColor: showRequests === 2 ? 'orange' : '#F0F0F5', color: 'black' }} label="Clients"></Tab>
 
                 </Tabs>
             </Box>
 
             {/* clients */}
-            {showRequests===2 && (
+            {showRequests === 2 && (
                 <div>
                     <div className="drop-down-box">
                         <Box sx={{ width: '150px', marginLeft: '1350px' }}>
@@ -167,8 +210,8 @@ const Clients = () => {
                                             <StyledTableCell align="center">{row.usability}</StyledTableCell>
                                             <StyledTableCell align="center">{row.status}</StyledTableCell>
                                             <StyledTableCell align="center">
-                                                <Button sx={{ color: 'white', backgroundColor: '#fe9e0d', ':hover': { backgroundColor: '#fe9e0d' } }}>Pets Details</Button>
-                                           </StyledTableCell>
+                                                <Button onClick={()=> viewPet()} sx={{ color: 'white', backgroundColor: 'orange', ':hover': { backgroundColor: 'orange' } }}>Pets Details</Button>
+                                            </StyledTableCell>
                                         </StyledTableRow>
                                     ))}
                                 </TableBody>
@@ -179,7 +222,7 @@ const Clients = () => {
             )}
 
             {/* clients request */}
-            {showRequests===0 && (
+            {showRequests === 0 && (
                 <div>
                     <div className="drop-down-box1">
                         <Box sx={{ width: '150px', marginLeft: '1350px' }}>
@@ -229,10 +272,10 @@ const Clients = () => {
                                             <StyledTableCell align="center">{secondrows.payment}</StyledTableCell>
                                             <StyledTableCell align="center">{secondrows.status}</StyledTableCell>
                                             <StyledTableCell align="center">
-                                                <Button sx={{ color: 'white', backgroundColor: '#fe9e0d', ':hover': { backgroundColor: '#fe9e0d' } }}>Pets Details</Button>
+                                                <Button onClick={()=> viewPet()} sx={{ color: 'white', backgroundColor: 'orange', ':hover': { backgroundColor: 'orange' } }}>Pets Details</Button>
                                             </StyledTableCell>
                                             <StyledTableCell align="center">
-                                                {secondrows.status === 'accepted' ? <Button sx={{ color: 'white', backgroundColor: '#000000', ':hover': { backgroundColor: '#555555' } }}>Completed</Button> : ""}
+                                                {secondrows.status === 'accepted' ? <Button sx={{ color: 'white', backgroundColor: '#000000', ':hover': { backgroundColor: '#000000' } }}>Completed</Button> : ""}
                                             </StyledTableCell>
                                         </StyledTableRow>
                                     ))}
@@ -244,7 +287,7 @@ const Clients = () => {
             )}
 
             {/* refund requests */}
-            {showRequests===1 && (
+            {showRequests === 1 && (
                 <div>
                     <div className="drop-down-box1">
                         <Box sx={{ width: '150px', marginLeft: '1350px' }}>
@@ -292,8 +335,10 @@ const Clients = () => {
                                             <StyledTableCell align="center">{refundrow.rqStatus}</StyledTableCell>
                                             <StyledTableCell align="center">{refundrow.rfStatus}</StyledTableCell>
                                             <StyledTableCell align="center">
-                                                {refundrow.rfStatus === 'completed' ? <Button sx={{ color: 'white', width:'80%', backgroundColor: '#000000', ':hover': { backgroundColor: '#555555' } }}>Refund Details</Button> 
-                                                :  <Button sx={{ color: 'white', width:'80%', backgroundColor: 'orange', ':hover': { backgroundColor: 'orange' } }}>Refund </Button> }
+                                                {refundrow.rfStatus === 'completed' 
+                                                    ? <Button onClick={()=> ViewRefundDetails()} sx={{ color: 'white', width: '80%', backgroundColor: '#000000', ':hover': { backgroundColor: '#555555' } }}>Refund Details</Button>
+                                                    : <Button onClick={() => RefundAdding()} sx={{ color: 'white', width: '80%', backgroundColor: 'orange', ':hover': { backgroundColor: 'orange' } }}>Refund </Button>}
+                                                    {/* : <Button onClick ={()=> addRefund()} sx={{ color: 'white', width: '80%', backgroundColor: 'orange', ':hover': { backgroundColor: 'orange' } }}>Refund </Button>} */}
                                             </StyledTableCell>
                                         </StyledTableRow>
                                     ))}
@@ -305,6 +350,136 @@ const Clients = () => {
             )}
 
             {/* pet details */}
+            {pet && (
+                <div>
+                    <FormControl sx={{ marginLeft: '30%', borderRadius: '10px', width: '700px', padding: '20px', backgroundColor: '#F0F0F5' }}>
+                        <div style={{ backgroundColor: 'white', paddingTop:'20px', paddingBottom:'20px', paddingRight: '60px', paddingLeft: '60px', borderRadius: '10px' }}>
+                            <div className="form-topic">
+                                 Pet Details
+                            </div>
+
+                            <div className="form-label">
+                                <img src={ PetImage } alt="pet image" style={{width:'200px', height:'auto', marginLeft:'180px'}} />
+                            </div>
+
+                            <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between'}}>
+                                <div className="form-label">
+                                    <FormLabel>  Pet ID : 02 </FormLabel>
+                                </div>
+
+                                <div className="form-label">
+                                    <FormLabel>  Pet Category : Dog </FormLabel>
+                                </div>
+                            </div>
+
+                            <div className="form-label">
+                                <FormLabel>Food Preference : <br /> </FormLabel>
+                                <ul style={{paddingRight:'20px', paddingLeft:'30px', paddingTop:'10px', paddingBottom:'10px', borderStyle:'solid', borderColor:'black', borderRadius:'10px'}}>
+                                    <li>This is food prefernece 01</li>
+                                    <li>This is food prefernece 02</li>
+                                    <li>This is food prefernece 03</li>
+                                </ul>
+                            </div>
+                            <div className="form-label">
+                                    <FormLabel>Owner Details :</FormLabel>
+                            </div>
+                               
+                            <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between'}}>
+                                <div className="form-label">
+                                    <FormLabel>Owner ID : 05 </FormLabel>
+                                </div>
+                                <div className="form-label">
+                                    <FormLabel>Name : John Doe </FormLabel>
+                                </div>
+                            </div>
+
+                            <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between'}}>
+                                <div className="form-label">
+                                        <img src={ OwnerImage } alt="owner image" style={{width:'100px', height:'auto'}} />
+                                </div>
+
+                                <div className="form-label">
+                                    <FormLabel>Contact Number : 0773214533 </FormLabel>
+                                    <FormLabel>Address :  No: 23, Main Street, Negombo </FormLabel>
+                                </div>
+                            </div>
+
+                            {/* <Button variant="contained" onClick={() => afterAddingComplain()} sx={{ background: 'orange', width: '100%', marginTop: '10px', ':hover': { backgroundColor: "#fe9e0d" } }}>Add Complain</Button> */}
+                            <Button variant="contained" onClick={() => FinishPetViewing()}  sx={{ background: 'orange', width: '100%', marginTop: '10px', ':hover': { backgroundColor: "#fe9e0d" } }}>OK</Button>
+                        </div>
+                    </FormControl>
+                </div>
+            )}
+
+            {/* place refund */}
+            {addRefund && (
+                <div>
+                    <FormControl sx={{ marginLeft: '30%', borderRadius: '10px', width: '700px', padding: '20px', backgroundColor: '#F0F0F5' }}>
+                        <div style={{ backgroundColor: 'white', paddingTop:'20px', paddingBottom:'20px', paddingRight: '60px', paddingLeft: '60px', borderRadius: '10px' }}>
+                            <div className="form-topic">
+                                Place Refund
+                            </div>
+
+                            <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between'}}>
+                                <div className="form-label">
+                                    <FormLabel>  Refund ID : 02 </FormLabel>
+                                </div>
+
+                                <div className="form-label">
+                                    <FormLabel>Request ID : 05 </FormLabel>
+                                </div>
+                            </div>
+
+                            <div className="form-label">
+                                <FormLabel>Upload Bank Slip : </FormLabel>
+                                <input type="file" placeholder=" Choose a file" variant="outlined" />
+                            </div>
+                            {/* <Button variant="contained" onClick={() => afterAddingComplain()} sx={{ background: 'orange', width: '100%', marginTop: '10px', ':hover': { backgroundColor: "#fe9e0d" } }}>Add Complain</Button> */}
+                            <Button variant="contained" onClick={()=> PlaceRefund()}  sx={{ background: 'orange', width: '100%', marginTop: '10px', ':hover': { backgroundColor: "#fe9e0d" } }}>Place Refund</Button>
+                        </div>
+                    </FormControl>
+                </div>
+            )}
+            
+            {/* view refund details */}
+            {viewRefund && (
+                <div>
+                    <FormControl sx={{ marginLeft: '30%', borderRadius: '10px', width: '700px', padding: '20px', backgroundColor: '#F0F0F5' }}>
+                        <div style={{ backgroundColor: 'white',paddingTop:'20px', paddingBottom:'20px', paddingRight: '60px', paddingLeft: '60px', borderRadius: '10px' }}>
+                            <div className="form-topic">
+                                View Refund Details
+                            </div>
+
+                            <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between'}}>
+                                <div className="form-label">
+                                    <FormLabel>  Refund ID : 02 </FormLabel>
+                                </div>
+
+                                <div className="form-label">
+                                    <FormLabel>Request ID : 05 </FormLabel>
+                                </div>
+                            </div>
+
+                            <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between'}}>
+                                <div className="form-label">
+                                    <FormLabel>  Refund Date : 2023/07/20 </FormLabel>
+                                </div>
+
+                                <div className="form-label">
+                                    <FormLabel>Refund Time : 10:00:00 </FormLabel>
+                                </div>
+                            </div>
+
+                            <div className="form-label">
+                                <FormLabel>Uploaded Bank Slip : </FormLabel>
+                                <img src={ BankSlip } alt="bank slip" style={{width:'100%', height:'auto', border:'solid black 1px', borderRadius:'10px'}} />
+                            </div>
+                            {/* <Button variant="contained" onClick={() => afterAddingComplain()} sx={{ background: 'orange', width: '100%', marginTop: '10px', ':hover': { backgroundColor: "#fe9e0d" } }}>Add Complain</Button> */}
+                            <Button variant="contained" onClick={() => FinishRefundViewing()} sx={{ background: 'orange', width: '100%', marginTop: '10px', ':hover': { backgroundColor: "#fe9e0d" } }}>Finish Viewing</Button>
+                        </div>
+                    </FormControl>
+                </div>
+            )}
 
         </div>
     );
