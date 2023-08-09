20e9feb5d7a252eee451be57a6d36f09210d1f11
gayya20 <92713181+gayya20@users.noreply.github.com>
2023-08-10 00:04:39 +0530

    add vide pet  care file

--- frontend/src/pages/Client/Petcare.js
+++ frontend/src/pages/Client/Petcare.js
@@ -1,28 +1,77 @@
-import * as React from 'react';
+
+import React, { useState, useEffect } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea, CardActions } from '@mui/material';
-import image1 from "../../assests/online-store-image.png"
-import image2 from "../../assests/care-center-image.png"
-import image3 from "../../assests/pic3.jpg"
+import image1 from "../../assests/online-store-image.png";
+import image2 from "../../assests/care-center-image.png";
+import image3 from "../../assests/pic3.jpg";
 import { Link } from 'react-router-dom';
+import video from "../../assests/Video3.mp4";
+import DownImage from "../../assests/down.png";
 
 import "../../styles/Client/Mindrelax.css";
 
-
-
-
-
-
 export default function Petcare() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const videoPlaybackRate = 0.2 + scrollPosition * 0.001; // Adjust the factor to control the effect
+  const videoOpacity = 0.9- scrollPosition * 0.002; // Adjust the factor to control the effect
+
   return (
     <>
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          backgroundColor: "black",
+          position: "relative",
+        }}
+      >
+        <video
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            opacity: videoOpacity,
+          }}
+          src={video}
+          autoPlay
+          loop
+          muted
+          playbackRate={videoPlaybackRate}
+        />
+        <div className="content">
+          <h1
+            style={{ fontSize: "80px", color: "white", marginTop: "-30vh" }}
+          >
+            Pet Care
+          </h1>
+          <p>keep your pet happy</p>
+          <p style={{marginTop:"200px"}}> OUR SERVISES</p>
+          <img src={DownImage} alt="Down Arrow" style={{ width: 40, height: 40,marginTop:"20px" }} /> {/* Use the image */}
 
- 
-    <h1 style={{textAlign:"center",fontSize:"60px",fontWeight:"40",marginTop:"30px"}}> Pet <span style={{color:"orange"}}>care</span> </h1>
-    <div className='smooth-scroll' style={{marginLeft:"180px",display:"flex",gap:"30px",marginTop:"35px"}}>
+
+        </div>
+      </div> 
+  <div className='smooth-scroll' style={{marginLeft:"180px",display:"flex",gap:"30px",marginTop:"35px"}}>
     <Card sx={{ maxWidth: 345 ,transition: "transform 0.5s ",
                 "&:hover": {
                   transform: "scale(1.1)", // Apply scale transform on hover
