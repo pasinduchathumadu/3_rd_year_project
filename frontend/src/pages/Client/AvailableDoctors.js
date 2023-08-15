cb373badf85e48861eef6116bfdf6fb6c91ef030
gayya20 <92713181+gayya20@users.noreply.github.com>
2023-08-15 21:07:38 +0530

    edit images and edit doctor list file

--- frontend/src/pages/Client/AvailableDoctors.js
+++ frontend/src/pages/Client/AvailableDoctors.js
@@ -1,10 +1,10 @@
 
-import product3  from '../../assests/c.png'
-import product4  from '../../assests/f.png'
-import product5  from '../../assests/cat1.png'
-import product6  from '../../assests/h.png'
-import product7  from '../../assests/cat2.png'
-import product8  from '../../assests/bird1.png'
+import product3  from '../../assests/vet-1.jpeg'
+import product4  from '../../assests/vet-2.png'
+import product5  from '../../assests/vet-3.png'
+import product6  from '../../assests/vet-4.jpeg'
+import product7  from '../../assests/vet-5.jpeg'
+import product8  from '../../assests/vet-6.jpg'
 import product9  from '../../assests/bird4.png'
 import product10  from '../../assests/j.png'
 
@@ -12,28 +12,28 @@ import product10  from '../../assests/j.png'
 export const DOCTORS =[
     {
         id : 1,
-        productName:"parrot",
+        productName:"Dr.Pasindu chathumadu",
         price:999.0,
-        productImages:product9,
+        productImages:product5,
     },
 
     {
         id : 2,
-        productName:"macBook pro",
+        productName:"Dr.Lasnidu",
         price:1999.0,
-        productImages:product10,
+        productImages:product4,
     },
 
     {
         id : 3,
-        productName:"canon camera",
+        productName:"Dr.Hashini",
         price:1799.0,
-        productImages:product3,
+        productImages:product6,
     },
 
     {
         id : 4,
-        productName:"Blue-jacket",
+        productName:"Dr.Gamage",
         price:500.0,
         productImages:product4,
     },
@@ -47,21 +47,21 @@ export const DOCTORS =[
 
     {
         id : 6,
-        productName:" Recommended T-shirt",
+        productName:" Dr.Saman",
         price:25.0,
         productImages:product6,
     },
 
     {
         id : 7,
-        productName:"Hifer Golf-shirt",
+        productName:"Dr.Chanaka",
         price:250.0,
         productImages:product7,
     },
 
     {
         id : 8,
-        productName:"Golden Rastha Cap",
+        productName:"Dr.silva",
         price:5.0,
         productImages:product8,
     }
