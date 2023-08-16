7dee69cf57f8827491f8d3b10d4ec391364ad0ae
Hashini Thilinika <hashinithilinika.av@gmail.com>
2023-08-16 12:08:50 +0530

    back button connect

--- frontend/src/pages/Client/ViewMedical.js
+++ frontend/src/pages/Client/ViewMedical.js
@@ -3,7 +3,9 @@ import React from 'react';
 import BackgroundImage from '../../assests/medical-reports.jpeg';
 import PDFIcon from '../../assests/pdf.jpeg';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import ProfilePhoto from '../../assests/oip.jpg';
+import { Link } from 'react-router-dom';
 
 const ViewMedical = () => {
     return (
@@ -16,112 +18,116 @@ const ViewMedical = () => {
                 <div style={{ margin: '10px', padding: '20px', backgroundColor: '#f0f0f5', borderRadius: '10px', marginTop: '30px' }}>
                     <FormControl>
                         <Typography sx={{ fontSize: '30px', marginLeft: '250px' }}>Pet Details </Typography>
-                        <Avatar src={ProfilePhoto} alt="profile" sx={{height:'100px', width:'100px', marginLeft:'270px'}} />
-                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent:'space-between' }}>
-                            <div>
-                                <Typography>Pet ID :</Typography>
-                                <Box
-                                    component="form"
-                                    sx={{
-                                        '& .MuiTextField-root': { m: 1, width: '25ch' },
-                                    }}
-                                    noValidate
-                                    autoComplete="off"
-                                >
-                                    <div>
-                                        <TextField
-                                            disabled
-                                            id="outlined-disabled"
-                                            // label="04"
-                                            defaultValue="04"
-                                        /></div>
-                                </Box>
-                            </div>
+                        <Link to="/medi">
+                        <Button variant="contained" sx={{ background: "orange", marginLeft: '500px', ':hover': { backgroundColor: "orange" }, width:'150px' }}>Back</Button>
+                        </Link>
 
-                            <div>
-                                <Typography>Pet Name :</Typography>
-                                <Box
-                                    component="form"
-                                    sx={{
-                                        '& .MuiTextField-root': { m: 1, width: '25ch' },
-                                    }}
-                                    noValidate
-                                    autoComplete="off"
-                                >
-                                    <div>
-                                        <TextField
-                                            disabled
-                                            id="outlined-disabled"
-                                            // label="Jimmy Boy"
-                                            defaultValue="Jimmy Boy"
-                                        /></div>
-                                </Box>
-                            </div>
-
-                        </div>
-                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent:'space-between' }}>
-                            <div>
-                                <Typography>Category :</Typography>
-                                <Box
-                                    component="form"
-                                    sx={{
-                                        '& .MuiTextField-root': { m: 1, width: '25ch' },
-                                    }}
-                                    noValidate
-                                    autoComplete="off"
-                                >
-                                    <div>
-                                        <TextField
-                                            disabled
-                                            id="outlined-disabled"
-                                            defaultValue="Dog"
-                                        /></div>
-                                </Box>
-                            </div>
-
-                            <div>
-                                <Typography>Breed :</Typography>
-                                <Box
-                                    component="form"
-                                    sx={{
-                                        '& .MuiTextField-root': { m: 1, width: '25ch' },
-                                    }}
-                                    noValidate
-                                    autoComplete="off"
-                                >
-                                    <div>
-                                        <TextField
-                                            disabled
-                                            id="outlined-disabled"
-                                            defaultValue="Jerman Shepard"
-                                        /></div>
-                                </Box>
-                            </div>
-
+                    <Avatar src={ProfilePhoto} alt="profile" sx={{ height: '100px', width: '100px', marginLeft: '270px' }} />
+                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <div>
+                            <Typography>Pet ID :</Typography>
+                            <Box
+                                component="form"
+                                sx={{
+                                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                                }}
+                                noValidate
+                                autoComplete="off"
+                            >
+                                <div>
+                                    <TextField
+                                        disabled
+                                        id="outlined-disabled"
+                                        // label="04"
+                                        defaultValue="04"
+                                    /></div>
+                            </Box>
                         </div>
 
-                        <div style={{ width: '650px', backgroundColor: 'white', borderRadius: '10px', padding: '10px', marginBottom: '15px' }}>
-                            <Typography sx={{ fontWeight: 'bold' }}>Vaccination Records :</Typography>
-                            <div>
-                                <img src={PDFIcon} alt="pdf" style={{ height: '70px', width: 'auto' }} />
-                            </div>
+                        <div>
+                            <Typography>Pet Name :</Typography>
+                            <Box
+                                component="form"
+                                sx={{
+                                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                                }}
+                                noValidate
+                                autoComplete="off"
+                            >
+                                <div>
+                                    <TextField
+                                        disabled
+                                        id="outlined-disabled"
+                                        // label="Jimmy Boy"
+                                        defaultValue="Jimmy Boy"
+                                    /></div>
+                            </Box>
                         </div>
 
-                        <div style={{ width: '650px', backgroundColor: 'white', borderRadius: '10px', padding: '10px', marginBottom: '15px' }}>
-                            <Typography sx={{ fontWeight: 'bold' }}>X -ray Records :</Typography>
-                            <div>
-                                <img src={PDFIcon} alt="pdf" style={{ height: '70px', width: 'auto' }} />
-                                <img src={PDFIcon} alt="pdf" style={{ height: '70px', width: 'auto' }} />
-                                <img src={PDFIcon} alt="pdf" style={{ height: '70px', width: 'auto' }} />
-
-                            </div>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <div>
+                            <Typography>Category :</Typography>
+                            <Box
+                                component="form"
+                                sx={{
+                                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                                }}
+                                noValidate
+                                autoComplete="off"
+                            >
+                                <div>
+                                    <TextField
+                                        disabled
+                                        id="outlined-disabled"
+                                        defaultValue="Dog"
+                                    /></div>
+                            </Box>
                         </div>
-                    </FormControl>
 
-                </div>
+                        <div>
+                            <Typography>Breed :</Typography>
+                            <Box
+                                component="form"
+                                sx={{
+                                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                                }}
+                                noValidate
+                                autoComplete="off"
+                            >
+                                <div>
+                                    <TextField
+                                        disabled
+                                        id="outlined-disabled"
+                                        defaultValue="Jerman Shepard"
+                                    /></div>
+                            </Box>
+                        </div>
+
+                    </div>
+
+                    <div style={{ width: '650px', backgroundColor: 'white', borderRadius: '10px', padding: '10px', marginBottom: '15px' }}>
+                        <Typography sx={{ fontWeight: 'bold' }}>Vaccination Records :</Typography>
+                        <div>
+                            <img src={PDFIcon} alt="pdf" style={{ height: '70px', width: 'auto' }} />
+                        </div>
+                    </div>
+
+                    <div style={{ width: '650px', backgroundColor: 'white', borderRadius: '10px', padding: '10px', marginBottom: '15px' }}>
+                        <Typography sx={{ fontWeight: 'bold' }}>X -ray Records :</Typography>
+                        <div>
+                            <img src={PDFIcon} alt="pdf" style={{ height: '70px', width: 'auto' }} />
+                            <img src={PDFIcon} alt="pdf" style={{ height: '70px', width: 'auto' }} />
+                            <img src={PDFIcon} alt="pdf" style={{ height: '70px', width: 'auto' }} />
+
+                        </div>
+                    </div>
+                </FormControl>
 
             </div>
+
         </div>
+        </div >
     )
 
 }
