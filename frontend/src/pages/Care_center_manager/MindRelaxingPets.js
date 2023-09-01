2019109268b97d784ea1b1adf72006fe9910f373
Hashini Thilinika <hashinithilinika.av@gmail.com>
2023-09-01 17:51:48 +0530

    care center-mind relaxing - view pets

--- frontend/src/pages/Care_center_manager/MindRelaxingPets.js
+++ frontend/src/pages/Care_center_manager/MindRelaxingPets.js
@@ -1,7 +1,14 @@
-import { Avatar, Typography } from "@mui/material";
+import { Avatar, Button, Typography } from "@mui/material";
 import { Stack } from "@mui/system";
 import React from "react";
 import profile from "../../assests/profile.jpg";
+import care from "../../assests/caregiver.jpg";
+import care2 from "../../assests/caregiver2.jpg";
+import AddIcon from "@mui/icons-material/Add";
+import StarIcon from "@mui/icons-material/Star";
+import CategoryIcon from '@mui/icons-material/Category';
+import PetsIcon from '@mui/icons-material/Pets';
+
 
 const MindRelaxingPets = () => {
     const input = new Date();
@@ -9,7 +16,7 @@ const MindRelaxingPets = () => {
 
     return (
         <div>
-            <div style={{ display: "flex", marginTop:'4%' }}>
+            <div style={{ display: "flex", marginTop: '4%' }}>
                 <div style={{ display: "inline", marginTop: "30px", marginLeft: "2%", width: "33.3%" }}>
                     <Typography>Care Center Manager</Typography>
                     <Typography>Today</Typography>
@@ -44,18 +51,74 @@ const MindRelaxingPets = () => {
                     </Stack>
                     </div>
                 </div>
-
-
             </div>
-        
-        
-        
-        {/* view pets for mind relaxing */}
 
-        {/* add pets for mind relaxing */}
+            {/* view pets for mind relaxing */}
+            <div style={{marginTop:'10px', marginBottom:'10px', marginLeft:'83%'}}>
+                <Button sx={{color:'white', backgroundColor:'black', ':hover':{backgroundColor:'black'}}}>Add New Pets<AddIcon /></Button>
+            </div>
+
+            <div className="row">
+                <div className="column">
+                    <div class="card">
+                        <img src={care} alt="John" className="top-img" />
+                        <Typography sx={{fontSize:'20px'}}><PetsIcon sx={{color:'orange'}} />Jimmy Boy</Typography><br />
+                        <Typography sx={{color:'#A9A9A9', fontWeight:'bold'}}>Male </Typography> <br />
+                        <Typography><CategoryIcon sx={{color: 'orange'}} /> Altheshion</Typography>
+                    </div>
+                </div>
+
+                <div className="column">
+                    <div class="card">
+                        <img src={care2} alt="John" className="top-img" />
+                        <Typography sx={{fontSize:'20px'}}><PetsIcon sx={{color:'orange'}} />Jimmy Boy</Typography><br />
+                        <Typography sx={{color:'#A9A9A9', fontWeight:'bold'}}>Male </Typography> <br />
+                        <Typography><CategoryIcon sx={{color: 'orange'}} /> Altheshion</Typography>
+                        
+                    </div>
+                </div>
+
+                <div className="column">
+                    <div class="card">
+                        <img src={care} alt="John" className="top-img" />
+                        <Typography sx={{fontSize:'20px'}}><PetsIcon sx={{color:'orange'}} />Jimmy Boy</Typography><br />
+                        <Typography sx={{color:'#A9A9A9', fontWeight:'bold'}}>Male </Typography> <br />
+                        <Typography><CategoryIcon sx={{color: 'orange'}} /> Altheshion</Typography>
+                    </div>
+                </div>
+
+                <div className="column">
+                    <div class="card">
+                        <img src={care2} alt="John" className="top-img" />
+                        <Typography sx={{fontSize:'20px'}}><PetsIcon sx={{color:'orange'}} />Jimmy Boy</Typography><br />
+                        <Typography sx={{color:'#A9A9A9', fontWeight:'bold'}}>Male </Typography> <br />
+                        <Typography><CategoryIcon sx={{color: 'orange'}} /> Altheshion</Typography>
+                    </div>
+                </div>
+
+                <div className="column">
+                    <div class="card">
+                        <img src={care} alt="John" className="top-img" />
+                        <Typography sx={{fontSize:'20px'}}><PetsIcon sx={{color:'orange'}} />Jimmy Boy</Typography><br />
+                        <Typography sx={{color:'#A9A9A9', fontWeight:'bold'}}>Male </Typography> <br />
+                        <Typography><CategoryIcon sx={{color: 'orange'}} /> Altheshion</Typography>
+                    </div>
+                </div>
+
+                <div className="column">
+                    <div class="card">
+                        <img src={care2} alt="John" className="top-img" />
+                        <Typography sx={{fontSize:'20px'}}><PetsIcon sx={{color:'orange'}} />Jimmy Boy</Typography><br />
+                        <Typography sx={{color:'#A9A9A9', fontWeight:'bold'}}>Male </Typography> <br />
+                        <Typography><CategoryIcon sx={{color: 'orange'}} /> Altheshion</Typography>
+                    </div>
+                </div>
+            </div>
+
+            {/* add pets for mind relaxing */}
 
 
-        
+
         </div>
     )
 }
