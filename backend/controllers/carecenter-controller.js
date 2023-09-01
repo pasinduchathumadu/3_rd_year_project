c01579d777c77dae0951937b14c9a2debe67e241
Hashini Thilinika <hashinithilinika.av@gmail.com>
2023-09-02 01:56:36 +0530

    care center - changes of bckend app.js , complains

--- backend/controllers/carecenter-controller.js
+++ backend/controllers/carecenter-controller.js
@@ -13,3 +13,14 @@ export const viewmycomplain = async (req, res, next) => {
         return res.json({ data })
     })
 }
+
+// view clients complains
+export const viewClientcomplains = async (req, res, next) => {
+    const sqlQuery = 'SELECT client_id, complain_id, complain_txt, com_date, com_time, complain_status, response_txt FROM client_complain WHERE manager_role = "care_center_manager" ';
+    db.query(sqlQuery, (err, data) => {
+        if (err) {
+            return res.json({ message: 'There is an internal error' })
+        }
+        return res.json({ data })
+    })
+}
