5932f8d16d2c293e92567a3f7f724d0d03044a0c
Hashini Thilinika <hashinithilinika.av@gmail.com>
2023-09-04 15:54:54 +0530

    manager profile, filtering

--- backend/routes/boarding-routes.js
+++ backend/routes/boarding-routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {addPackage, getPackage, view_requests, view_allclients, add_complain, viewmyComplains, viewClientsComplains, packageUsage, viewCurrent, viewRequested, viewBoarded, refund_requests, countPets, pendingRequest, view_refundDetails,toRefund, viewPetDetails,refundAdding, complainDetails, addingResponse, deleteMyComplain,AcceptedtoArrived, ArrviedtoCompleted} from '../controllers/boarding-controller';
+import {addPackage, getPackage, view_requests, view_allclients, add_complain, viewmyComplains, viewClientsComplains, packageUsage, viewCurrent, viewRequested, viewBoarded, refund_requests, countPets, pendingRequest, view_refundDetails,toRefund, viewPetDetails,refundAdding, complainDetails, addingResponse, deleteMyComplain,AcceptedtoArrived, ArrviedtoCompleted,filterbox1,filterbox2} from '../controllers/boarding-controller';
 
 const boarding_house_manager = express.Router()
 
@@ -37,6 +37,13 @@ boarding_house_manager.get('/viewBoarded',viewBoarded)
 boarding_house_manager.get('/countPets',countPets)
 boarding_house_manager.get('/packageUsage',packageUsage)
 boarding_house_manager.get('/pendingRequest',pendingRequest)
+
+// FILTERING
+// dashboard 
+boarding_house_manager.get('/filterbox1/:id',filterbox1)
+boarding_house_manager.get('/filterbox2/:id',filterbox2)
+
+
 
 
 
