359aadc566dd0d3b423fcfa698e1ec89dfdbb8a8
Hashini Thilinika <hashinithilinika.av@gmail.com>
2023-10-27 19:55:01 +0530

    pet profiles changes

--- frontend/src/pages/Medi-help_manager/PetProfile.js
+++ frontend/src/pages/Medi-help_manager/PetProfile.js
@@ -1,12 +1,10 @@
-
 import React, { useEffect, useState } from "react";
-
 import '../../styles/Medi-help_manager/PetProfile.css';
-import DeletePet from "./DeletePet";
-import AddPet from "./AddPet";
-import AddVaccine from "./AddVaccine";
-import ViewVaccine from "./ViewVaccine";
-import ViewProfile from "./ViewProfile";
+// import DeletePet from "./DeletePet";
+// import AddPet from "./AddPet";
+// import AddVaccine from "./AddVaccine";
+// import ViewVaccine from "./ViewVaccine";
+// import ViewProfile from "./ViewProfile";
 import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -14,28 +12,29 @@ import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import { Button, Typography, MenuItem, Select, InputLabel, IconButton, FormLabel, Box, TextField } from "@mui/material";
+import { Button, Typography, MenuItem, Select, InputLabel, IconButton, FormLabel, Box, TextField, Stack, Alert } from "@mui/material";
 import { useNavigate } from "react-router";
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import Paper from '@mui/material/Paper';
 import { FormControl } from "@mui/base";
 import CloseIcon from '@mui/icons-material/Close';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+// import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import pdficon from '../../assests/pdficon.png'
 import axios from "axios";
+import { format } from 'date-fns';
+
 
 
 const PetProfile = () => {
   const navigate = useNavigate("")
-  const [show, setShow] = useState(false);
-  const [model2Open, setModel2Open] = useState(false);
-  const [model3Open, setModel3Open] = useState(false);
-  const [model4Open, setModel4Open] = useState(false);
-  const [modelOpen, setModelOpen] = useState(false);
+  // const [show, setShow] = useState(false);
+  // const [model2Open, setModel2Open] = useState(false);
+  // const [model3Open, setModel3Open] = useState(false);
+  // const [model4Open, setModel4Open] = useState(false);
+  // const [modelOpen, setModelOpen] = useState(false);
 
   const input = new Date();
   const date = input.toDateString();
-
 
   const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -57,17 +56,17 @@ const PetProfile = () => {
     },
   }));
 
-  function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-  }
+  // function createData(name, calories, fat, carbs, protein) {
+  //   return { name, calories, fat, carbs, protein };
+  // }
 
-  const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-  ];
+  // const rows = [
+  //   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  //   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  //   createData('Eclair', 262, 16.0, 24, 6.0),
+  //   createData('Cupcake', 305, 3.7, 67, 4.3),
+  //   createData('Gingerbread', 356, 16.0, 49, 3.9),
+  // ];
 
   // connect profile 
   const profile = () => {
@@ -77,7 +76,6 @@ const PetProfile = () => {
   // get profile picture
   const getProfilepicturepath = (imageName) => {
     return require(`../../../../backend/images/store/${imageName}`)
-
   }
 
   const [main, setmain] = useState(true)
@@ -85,6 +83,24 @@ const PetProfile = () => {
   const [nextVaccine, setnextVaccine] = useState(false)
   const [pastVaccine, setpastVaccine] = useState(false)
   const [other, setother] = useState(false)
+
+  // get details for main page
+  const [maindata, setmaindata] =useState("")
+  const getDetails = async() => {
+    try {
+      const res = await axios.get(`http://localhost:5000/pet_care/medi_help_manager/getDetails`)
+      const data = await res.data
+      return data
+
+    }catch(err){
+      console.log(err)
+    }
+  }
+  useEffect(() => {
+    getDetails()
+    .then((data) =>setmaindata(data.data))
+    .catch((err) => console.log(err))
+  })
 
   //get pet ids from medi appointments
   const [addmedical, setaddmedical] = useState("")
@@ -94,44 +110,83 @@ const PetProfile = () => {
       setaddmedical(res.data.data)
       setmain(false)
       setadd(true)
+      seterror(false)
     } catch (err) {
       console.log('There is an internal error')
     }
   }
-  // submit add medical reports box
+
+  // get vaccine details
+  const [vacc, setvacc] = useState("")
+  const getVaccineDetails = async () => {
+    try {
+      const res = await axios.get(`http://localhost:5000/pet_care/medi_help_manager/getVaccineDetails`)
+      const data = await res.data
+      return data
+    } catch (err) {
+      console.log('There is an internal error')
+    }
+  }
+  useEffect(() => {
+    getVaccineDetails()
+      .then((data) => setvacc(data.data))
+      .catch((err) => console.log(err))
+  })
+
+
+  // submit add vaccine
   const [selectpetid, setselectpetid] = useState("")
+  const [selectvaccine, setselectvaccine] = useState("")
+  const [vaccinedate, setvaccinedate] = useState("")
   const [message, setmessage] = useState("")
   const [error, seterror] = useState(false)
-  const handlepetid = (event) => {
+  const date_today = format(input, 'yyy-MM-dd')
+
+  const handlePet = (event) => {
     setselectpetid(event.target.value)
   }
+  const handleVaccine = (event) => {
+    setselectvaccine(event.target.value)
+  }
+
+  // submit add vaccine form 
   const submitAddMedical = async () => {
+    if (selectpetid === "" || selectvaccine === "" || vaccinedate === "") {
+      seterror(true)
+      setmessage("Please enter all required fields")
+      return;
+    }
+    if (vaccinedate > date_today) {
+      seterror(true)
+      setmessage("Please enter a valid date")
+      return;
+    }
     try {
       const res = await axios.post(`http://localhost:5000/pet_care/medi_help_manager/submitAddMedical`, {
-        selectpetid
+        selectpetid,
+        selectvaccine,
+        vaccinedate,
       })
       if (res.data.message === 'There is an internal error') {
         setmessage('Internal error')
         seterror(true)
       } else if (res.data.message === 'success') {
         setmain(true)
-        setaddmedical(false)
+        setadd(false)
       }
     } catch (err) {
       console.log('There is an internal error')
     }
   }
 
-
-
-  // cancel adding medical reports
+  // cancel adding vaccine records
   const cancelAdding = () => {
     setadd(false)
     setmain(true)
   }
 
   // view next vaccine details
-  const nextVaccineDetails = () => {
+  const nextVaccineDetails = (id) => {
     setnextVaccine(true)
     setmain(false)
   }
@@ -143,26 +198,27 @@ const PetProfile = () => {
   }
 
   // view past vaccine details
-  const pastVaccinationDetails = () => {
-    setpastVaccine(true)
-    setmain(false)
+  // const [id, setid] = useState("")
+  // const [error1, seterror1] = useState(false)
+  // const [message1, setmessage1] = useState("")
+  // const [past,setpast] = useState("")
+
+  const pastVaccinationDetails = async (id) => {
+    //   try {
+    //     const res = await axios.get(`http://localhost:5000/pet_care/medi_help_manager/pastVaccinationDetails/${id}`)
+    //     setpast(res.data.data)
+    //     setid(id)
+    //     setmain(false)
+    //     /// seterror1(false)
+    //     setpastVaccine(true)      
+    //   } catch (err) {
+    //     console.log(err)
+    //   }
   }
 
   // close past vaccination details box
   const closePastVaccination = () => {
     setpastVaccine(false)
-    setmain(true)
-  }
-
-  // open other medical rpeorts box
-  const openOther = () => {
-    setother(true)
-    setmain(false)
-  }
-
-  // close other medical reports box
-  const closeOther = () => {
-    setother(false)
     setmain(true)
   }
 
@@ -198,9 +254,9 @@ const PetProfile = () => {
 
       {main && (
         <>
-          <div style={{ display:'flex', flexDirection:'row', justifyContent:'space-between', marginLeft:'2%', marginRight:'2%' }}>
-            <Button sx={{ color: 'white', backgroundColor: 'orange', ':hover': { backgroundColor: 'orange' }}} onClick={vaccineShedule} >Vaccination Shedule</Button>
-            <Button sx={{ color: 'white', backgroundColor: 'black', ':hover': { backgroundColor: 'black' }}} onClick={addMedical}>Add Vaccine Records </Button>
+          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginLeft: '2%', marginRight: '2%' }}>
+            <Button sx={{ color: 'white', backgroundColor: 'orange', ':hover': { backgroundColor: 'orange' } }} onClick={vaccineShedule} >Vaccination Shedule</Button>
+            <Button sx={{ color: 'white', backgroundColor: 'black', ':hover': { backgroundColor: 'black' } }} onClick={addMedical}>Add Vaccine Records </Button>
           </div>
 
           <div style={{ padding: '2%' }}>
@@ -209,27 +265,25 @@ const PetProfile = () => {
                 <TableHead>
                   <TableRow>
                     <StyledTableCell align="center">Pet ID</StyledTableCell>
-                    <StyledTableCell align="center">Client Email Address </StyledTableCell>
+                    {/* <StyledTableCell align="center">Client Email Address </StyledTableCell> */}
                     <StyledTableCell align="center">Next Vaccination</StyledTableCell>
                     <StyledTableCell align="center">Past Vaccinations</StyledTableCell>
-                    <StyledTableCell align="center">Other</StyledTableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
 
-                  <StyledTableRow>
-                    <StyledTableCell align="center">clientrow.complain_id</StyledTableCell>
-                    <StyledTableCell align="center">"clientrow.client_id"</StyledTableCell>
-                    <StyledTableCell align="center">
-                      <Button onClick={nextVaccineDetails} sx={{ backgroundColor: 'black', color: 'white', ':hover': { backgroundColor: 'black' }, width: '40%' }}>View</Button>
-                    </StyledTableCell>
-                    <StyledTableCell align="center">
-                      <Button onClick={pastVaccinationDetails} sx={{ backgroundColor: 'black', color: 'white', ':hover': { backgroundColor: 'black' }, width: '40%' }}>View</Button>
-                    </StyledTableCell>
-                    <StyledTableCell align="center">
-                      <Button onClick={openOther} sx={{ backgroundColor: 'black', color: 'white', ':hover': { backgroundColor: 'black' } }}>Medical Reports</Button>
-                    </StyledTableCell>
-                  </StyledTableRow>
+                  {maindata && maindata.map((menu, index) => (
+                    <StyledTableRow>
+                      <StyledTableCell align="center">{menu.pet_id}</StyledTableCell>
+                      {/* <StyledTableCell align="center">"clientrow.client_id"</StyledTableCell> */}
+                      <StyledTableCell align="center">
+                        <Button onClick={() => nextVaccineDetails(menu.pet_id)} sx={{ backgroundColor: 'black', color: 'white', ':hover': { backgroundColor: 'black' }, width: '40%' }}>View</Button>
+                      </StyledTableCell>
+                      <StyledTableCell align="center">
+                        <Button onClick={pastVaccinationDetails} sx={{ backgroundColor: 'black', color: 'white', ':hover': { backgroundColor: 'black' }, width: '40%' }}>View</Button>
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  ))}
                 </TableBody>
               </Table>
             </TableContainer>
@@ -237,6 +291,7 @@ const PetProfile = () => {
         </>
       )}
 
+      {/* add new vaccine record */}
       {add && (
         <div style={{
           backgroundColor: '#f0f0f5',
@@ -246,7 +301,7 @@ const PetProfile = () => {
           paddingBottom: '2%',
           marginLeft: '22%',
           marginRight: '3%',
-          marginTop: '2%',
+          marginTop: '0%',
           width: '50%',
           borderRadius: '2%'
         }}
@@ -260,7 +315,7 @@ const PetProfile = () => {
             }} /></IconButton>
           </div>
 
-          <Typography variant="h6" sx={{ textAlign: 'center', fontWeight: 'bold' }}>Adding Medical Reports</Typography>
+          <Typography variant="h6" sx={{ textAlign: 'center', fontWeight: 'bold' }}>Add New Vaccine Records</Typography>
           <hr />
           <InputLabel sx={{ textAlign: 'center' }}><span style={{ color: 'red' }}>**</span>Here display only pet ids from medi appointments</InputLabel>
 
@@ -270,13 +325,13 @@ const PetProfile = () => {
               <Select
                 labelId="demo-simple-select-standard-label"
                 id="demo-simple-select-standard"
-                value={addmedical}
-                // onChange={handleChangeCategory}
+                value={selectpetid}
+                onChange={handlePet}
                 label="Pet Category"
                 sx={{ width: '50%' }}
               >
                 {addmedical && addmedical.map((menu, index) => (
-                  <MenuItem key={index} value={menu.appointment_id}>
+                  <MenuItem key={index} value={menu.pet_id}>
                     {menu.pet_id}
                   </MenuItem>
                 ))}
@@ -284,35 +339,40 @@ const PetProfile = () => {
             </FormControl>
           </div>
 
-          {/* vaccnie id - name
-          date
-           */}
+          <div style={{ marginBottom: '1%', marginTop: '1%' }}>
+            <FormControl sx={{ minWidth: 120, width: '100%' }}>
+              <Typography>Vaccine</Typography>
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={selectvaccine}
+                onChange={handleVaccine}
+                label="Vaccine"
+                sx={{ width: '50%' }}
+              >
+                {vacc && vacc.map((menu, index) => (
+                  <MenuItem key={index} value={menu.vaccine_id}>
+                    {menu.vaccine_id}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+
           <div>
-
-
+            <Typography>Vaccined Date</Typography>
+            <TextField type="date" sx={{ width: '50%' }} onChange={(e) => setvaccinedate(e.target.value)} />
           </div>
 
 
-
-          {/* <div style={{ marginBottom: '1%', marginTop: '1%' }}>
-            <FormControl>
-              <Typography>Upload Medical Files :</Typography>
-              <div>
-                <Button
-                  variant="contained"
-                  component="label"
-                  startIcon={<CloudUploadIcon />}
-                  sx={{ width: '100%' }}
-                >
-                  Upload File
-                  <input type="file" hidden required />
-                </Button>
-              </div>
-            </FormControl>
-          </div> */}
+          {error && (
+            <Stack sx={{ width: '100%', marginTop: '1%', marginBottom: '1%' }} spacing={2}>
+              <Alert severity="error">{message}</Alert>
+            </Stack>
+          )}
 
           <div style={{ marginTop: '3%', }}>
-            <Button sx={{ backgroundColor: 'black', color: 'white', ':hover': { backgroundColor: 'black' }, width: '30%', marginLeft: '35%' }}>Submit</Button>
+            <Button sx={{ backgroundColor: 'black', color: 'white', ':hover': { backgroundColor: 'black' }, width: '30%', marginLeft: '35%' }} onClick={submitAddMedical}>Submit</Button>
           </div>
         </div>
       )}
@@ -428,55 +488,6 @@ const PetProfile = () => {
           </div>
         </div>
       )}
-
-
-      {/* other medical reports viewing */}
-      {other && (
-        <div style={{
-          backgroundColor: '#f0f0f5',
-          paddingLeft: '2%',
-          paddingRight: '2%',
-          paddingTop: '1%',
-          paddingBottom: '2%',
-          marginLeft: '28%',
-          marginRight: '3%',
-          marginTop: '2%',
-          width: '40%',
-          borderRadius: '2%'
-        }}
-        >
-          <div style={{ marginLeft: '94%' }}>
-            <IconButton onClick={closeOther}><CloseIcon sx={{
-              backgroundColor: 'red',
-              color: 'white',
-              marginLeft: '80%'
-            }} /></IconButton>
-          </div>
-
-          <Typography variant="h6" sx={{ textAlign: 'center', fontWeight: 'bold' }}>Other Medical Records</Typography>
-          <hr />
-
-          <div style={{ marginBottom: '1%', marginTop: '1%', display: 'flex', flexDirection: 'row' }}>
-            <Typography sx={{ fontWeight: 'bold', marginRight: '1%' }}>Pet ID:  </Typography>
-            <Typography sx={{ fontWeight: 'bold', marginLeft: '1%' }}>34  </Typography>
-          </div>
-          <hr />
-
-          <div style={{ marginBottom: '1%', marginTop: '2%', marginLeft: '5%' }}>
-            <img src={pdficon} style={{ width: '10%', marginRight: '2%' }} />
-            <img src={pdficon} style={{ width: '10%', marginRight: '2%' }} />
-            <img src={pdficon} style={{ width: '10%', marginRight: '2%' }} />
-
-          </div>
-        </div>
-      )}
-
-
-
-
-
-
-
     </div>
   )
 }
